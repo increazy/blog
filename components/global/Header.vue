89a5ca65af5e849111c3e4ae28afff1a02261ed7
Leonardo Vilarinho <leonardo-i@outlook.com>
2021-05-28 11:43:04 -0300

    update

--- components/global/Header.vue
+++ components/global/Header.vue
@@ -1,26 +1,240 @@
 <template>
-  <nav class="scrim-bg fixed z-40 top-0 inset-x-0 pt-3 px-3" aria-label="Main Menu">
-    <ul class="flex">
-      <li class="flex-1">
-        <nuxt-link class="btn block" to="/">Home</nuxt-link>
-      </li>
-      <li class="flex-1 ml-2">
-        <nuxt-link class="btn block" to="/blog">Blog</nuxt-link>
-      </li>
-      <li class="flex-1 ml-2">
-        <nuxt-link class="btn block" to="/projects">Projects</nuxt-link>
-      </li>
-    </ul>
+  <nav class="scrim-bg fixed z-40 top-0 inset-x-0 pt-3 px-3 inc-header" aria-label="Main Menu">
+    <div class="inc-header__menu">
+      <div class="inc-header__menu-hamburger-icon" @click="toggleMenu()">
+        <span></span>
+        <span class="second"></span>
+        <span></span>
+      </div>
+
+      <div class="inc-header__menu-ul-back" @click="toggleMenu()"></div>
+      <ul class="inc-header__menu-ul">
+        <li class="flex inc-header__menu-li" @click="toggleMenu()">
+          <nuxt-link class="btn block" to="/docs">Documentação</nuxt-link>
+        </li>
+        <li class="flex inc-header__menu-li" @click="toggleMenu()">
+          <nuxt-link class="btn block" to="/blog">Blog</nuxt-link>
+        </li>
+      </ul>
+    </div>
+
+    <div class="inc-header__search">
+      <div class="inc-header__search-form">
+        <input type="text" v-model="searchTerm" placeholder="o que está buscando?" />
+        <img src="~/assets/imgs/icons/icon-search.svg" alt="Search" />
+      </div>
+
+      <div class="inc-header__search-results" v-show="searchTerm">
+        <ul class="inc-header__search-results-ul">
+          <li class="inc-header__search-results-li" v-for="i in 10" :key="i"><a href="#">Resultado 1</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <nuxt-link class="inc-header__logo" to="/">
+      <img src="~/assets/imgs/logo.png" alt="Increazy" class="isdesktop" />
+      <img src="~/assets/imgs/logo-icon.png" alt="Increazy" class="ismobile" />
+    </nuxt-link>
   </nav>
 </template>
 
 <script>
 export default {
-  name: 'Header'
+  name: 'Header',
+  data() {
+    return {
+      searchTerm: '',
+    }
+  },
+  methods: {
+    toggleMenu() {
+      const menu = document.querySelector('.inc-header__menu-ul')
+      const back = document.querySelector('.inc-header__menu-ul-back')
+      const bread = document.querySelector('.inc-header__menu-hamburger-icon')
+
+      menu.classList.toggle('inc-header__menu-ul--active')
+      back.classList.toggle('inc-header__menu-ul-back--active')
+      bread.classList.toggle('inc-header__menu-hamburger-icon--active')
+    },
+  },
 }
 </script>
-
+ 
 <style lang="postcss" scoped>
+.inc-header {
+  width: 100%;
+  display: flex;
+  flex-flow: row;
+  justify-content: center;
+  align-items: center;
+}
+.inc-header__menu {
+  display: flex;
+  position: absolute;
+  top: 12px;
+  left: 12px;
+}
+.inc-header__menu-hamburger-icon {
+  display: flex;
+  width: 40px;
+  height: 38px;
+  flex-flow: column;
+  justify-content: space-between;
+  align-items: flex-start;
+  cursor: pointer;
+  z-index: 100;
+  padding: 5px;
+  border: 1px solid var(--border-color);
+  border-radius: 0.25rem;
+}
+.inc-header__menu-hamburger-icon span {
+  width: 100%;
+  height: 3px;
+  background: var(--color-primary-800);
+  border-radius: 20px;
+  transition: all 0.3s;
+}
+.inc-header__menu-hamburger-icon span:nth-child(2) {
+  width: 80%;
+  transition: all 0.3s;
+}
+.inc-header__menu-hamburger-icon:hover span:nth-child(2) {
+  width: 100% !important;
+  transition: all 0.3s;
+}
+.inc-header__menu-hamburger-icon--active span:nth-child(2) {
+  width: 100% !important;
+  transition: all 0.3s;
+}
+
+.inc-header__menu-ul-back {
+  position: fixed;
+  width: 0;
+  height: 0;
+  z-index: -98;
+}
+.inc-header__menu-ul-back--active {
+  height: 100% !important;
+  width: 100% !important;
+  z-index: 98 !important;
+}
+
+.inc-header__menu-ul {
+  width: 235px;
+  background: #fff;
+  padding: 0;
+  border-radius: 0.25rem;
+  box-shadow: 3px 3px 15px 0 rgb(0 0 0 / 20%);
+  position: absolute;
+  top: 43px;
+  left: 0;
+  transition: all 0.3s;
+  overflow: hidden;
+  height: 0;
+  z-index: -99;
+}
+.inc-header__menu-ul--active {
+  padding: 10px !important;
+  height: auto !important;
+  z-index: 99 !important;
+  transition: all 0.3s;
+}
+
+.inc-header__menu-li .btn {
+  width: 100%;
+  background: unset !important;
+  border: 0;
+  transition: all 0.3s;
+  cursor: pointer;
+}
+.inc-header__menu-li .btn:hover {
+  transition: all 0.3s;
+  background-color: var(--color-primary-800) !important;
+  color: #fff;
+}
+
+.inc-header__search {
+  position: absolute;
+  top: 12px;
+  left: 105px;
+}
+.inc-header__search-form {
+  width: 100%;
+  display: flex;
+  flex-flow: row;
+  justify-content: flex-start;
+  align-items: center;
+  position: relative;
+}
+.inc-header__search-form input {
+  background: transparent;
+  padding: 7px 30px 7px 10px;
+  height: 38px;
+}
+.inc-header__search-form img {
+  width: 20px;
+  position: absolute;
+  top: 9px;
+  right: 9px;
+}
+
+.inc-header__search-results {
+  position: absolute;
+  top: 40px;
+  width: 100%;
+  background: #fff;
+  border-radius: 0.25rem;
+  box-shadow: 0 0 15px -2px rgb(0 0 0 / 20%);
+  padding: 10px;
+}
+.inc-header__search-results-ul {
+  display: flex;
+  flex-flow: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  max-height: 300px;
+  overflow-y: auto;
+}
+.inc-header__search-results-li {
+  font-size: 14px;
+  margin: 5px 0;
+}
+.inc-header__search-results-li a:hover {
+  text-decoration: underline;
+}
+
+.inc-header__logo img {
+  width: 180px;
+}
+.inc-header__logo {
+  background: unset !important;
+  cursor: pointer;
+}
+
+@media (max-width: 1024px) {
+  .inc-header {
+    justify-content: space-between;
+  }
+  .inc-header__menu {
+    position: relative !important;
+    top: 0;
+    left: 0;
+  }
+  .inc-header__search {
+    position: relative !important;
+    width: 60%;
+    top: 0;
+    left: 0;
+  }
+  .inc-header__search-form input {
+    width: 100%;
+  }
+  .inc-header__logo img {
+    width: auto;
+    height: 40px;
+  }
+}
+
 .scrim-bg {
   &::before {
     content: '';
@@ -55,11 +269,19 @@ export default {
 
 /* Need two because of smoother switching between color modes */
 @keyframes fadeIn1 {
-  from { opacity: 0; }
-  to { opacity: 1; }
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
 }
 @keyframes fadeIn2 {
-  from { opacity: 0; }
-  to { opacity: 1; }
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
 }
 </style>
--- components/posts/posts.vue
+++ components/posts/posts.vue
@@ -1,34 +1,17 @@
 <template>
-  <ul v-if="posts.length > 0" class="cards">
-    <li
-      v-for="(post, index) in posts"
-      :key="index"
-    >
-      <nuxt-link
-        :to="`${postType}/${post.slug}`"
-        class="card card--clickable"
-      >
-        <template v-if="postType === 'projects'">
-          <span class="flex-1">
-            <h6 class="inline-block py-1 px-2 mr-1 bg-gray text-white text-sm font-medium rounded-sm">{{ post.category }}</h6>
-            <h3 class="card-title">{{ post.title }}</h3>
-            <p class="mt-2">{{ post.description }}</p>
-          </span>
-          <img
-            v-if="post.cover"
-            class="cover-image"
-            :src="post.cover"
-          >
-        </template>
-
-        <template v-else>
+  <ul v-if="posts.length > 0 && postType == 'blog'" class="cards inc-blogs">
+    <li v-for="(post, index) in posts" :key="index">
+      <nuxt-link :to="`${postType}/${post.slug}`" class="card card--clickable">
+        <template>
           <span class="w-full">
             <span class="flex justify-between align-baseline">
               <h3 class="card-title">{{ post.title }}</h3>
               <h6
                 v-if="post.createdAt"
                 class="self-start inline-block mt-0 py-1 px-2 bg-gray text-white text-base font-medium rounded-sm whitespace-no-wrap"
-              >{{ formatDate(post.createdAt) }}</h6>
+              >
+                {{ formatDate(post.createdAt) }}
+              </h6>
             </span>
             <p class="mt-2">{{ post.description }}</p>
           </span>
@@ -36,6 +19,40 @@
       </nuxt-link>
     </li>
   </ul>
+
+  <div v-else-if="posts.length > 0 && postType == 'docs'" class="inc-docs">
+    <div v-for="tag in tags" :key="tag" class="inc-docs__tags" :data-tag="tag.value">
+      <div class="inc-docs__title" @click="toggleTag(tag.value)">
+        <span class="inc-docs__title-text">{{ tag.label }}</span>
+        <div class="inc-docs__title-icon"><span>+</span></div>
+      </div>
+
+      <ul class="cards">
+        <li v-for="(post, index) in posts" :key="index" class="card-li" v-if="tag.value == post.tags">
+          <nuxt-link :to="`${postType}/${post.slug}`" class="card card--clickable">
+            <template>
+              <span class="w-full">
+                <span class="flex justify-between align-baseline">
+                  <h3 class="card-title">{{ post.title }}</h3>
+                  <h6
+                    v-if="post.createdAt"
+                    class="self-start inline-block mt-0 py-1 px-2 bg-gray text-white text-base font-medium rounded-sm whitespace-no-wrap"
+                  >
+                    {{ formatDate(post.createdAt) }}
+                  </h6>
+                  <div class="inc-docs__tag">
+                    <small> #{{ post.tags }} </small>
+                  </div>
+                </span>
+                <p class="mt-2">{{ post.description }}</p>
+              </span>
+            </template>
+          </nuxt-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+
   <div v-else-if="loading" class="cards">
     <div v-for="placeholder in placeholderClasses" :key="placeholder.id" class="card">
       <content-placeholders :rounded="true" :class="placeholder">
@@ -43,69 +60,206 @@
       </content-placeholders>
     </div>
   </div>
+
   <p v-else class="max-w-5xl mx-auto">
     {{ amount > 1 ? 'Posts not found' : 'Post not found' }}
   </p>
 </template>
 
 <script>
-  export default {
-    name: 'Posts',
-    props: {
-      postType: {
-        type: String,
-        default: 'blog',
-        validator: (val) => ['blog', 'projects'].includes(val),
-      },
-      amount: { // ? https://content.nuxtjs.org/fetching#limitn
-        type: Number,
-        default: 10,
-        validator: (val) => val >= 0 && val < 100,
-      },
-      sortBy: { // ? https://content.nuxtjs.org/fetching#sortbykey-direction
-        type: Object,
-        default: () => ({
-          key: 'slug',
-          direction: 'desc' // you probably want 'asc' here
-        }),
-        validator: (obj) => typeof obj.key === 'string' && typeof obj.direction === 'string',
+export default {
+  name: 'Posts',
+  props: {
+    postType: {
+      type: String,
+      default: 'blog',
+      validator: (val) => ['blog', 'projects', 'docs'].includes(val),
+    },
+    amount: {
+      // ? https://content.nuxtjs.org/fetching#limitn
+      type: Number,
+      default: 10,
+      validator: (val) => val >= 0 && val < 100,
+    },
+    sortBy: {
+      // ? https://content.nuxtjs.org/fetching#sortbykey-direction
+      type: Object,
+      default: () => ({
+        key: 'slug',
+        direction: 'desc', // you probably want 'asc' here
+      }),
+      validator: (obj) => typeof obj.key === 'string' && typeof obj.direction === 'string',
+    },
+  },
+  data() {
+    return {
+      posts: [],
+      loading: true,
+      tags: [
+        { label: 'Hooks', value: 'hooks' },
+        { label: 'Páginas', value: 'paginas' },
+        { label: 'Componentes', value: 'componentes' },
+        { label: 'Loading Skeleton', value: 'skeleton' },
+      ],
+    }
+  },
+  computed: {
+    placeholderClasses() {
+      const classes = ['w-full', 'w-2/3', 'w-5/6']
+      return [...Array.from({ length: this.amount }, (v, i) => classes[i % classes.length])] // repeats classes after one another
+    },
+  },
+  async mounted() {
+    this.loading = true
+    this.posts = await this.fetchPosts()
+    this.loading = false
+  },
+  methods: {
+    formatDate(dateString) {
+      const date = new Date(dateString)
+      return date.toLocaleDateString(process.env.lang) || ''
+    },
+    async fetchPosts(postType = this.postType, amount = this.amount, sortBy = this.sortBy) {
+      return this.$content(postType)
+        .sortBy(sortBy.key, sortBy.direction)
+        .limit(amount)
+        .fetch()
+        .catch((err) => {
+          error({ statusCode: 404, message: amount > 1 ? 'Posts not found' : 'Post not found' })
+        })
+    },
+    toggleTag(value) {
+      console.log(value)
+      const tag = document.querySelector(`.inc-docs__tags[data-tag="${value}"]`)
+      if (tag) {
+        tag.classList.toggle('inc-docs__tags--active')
       }
     },
-    data() {
-      return {
-        posts: [],
-        loading: true,
-      }
-    },
-    computed: {
-      placeholderClasses() {
-        const classes = ['w-full','w-2/3','w-5/6'];
-        return [...Array.from({ length: this.amount }, (v, i) => classes[i % classes.length])]; // repeats classes after one another
-      }
-    },
-    async mounted() {
-      this.loading = true;
-      this.posts = await this.fetchPosts();
-      this.loading = false;
-    },
-    methods: {
-      formatDate(dateString) {
-        const date = new Date(dateString)
-        return date.toLocaleDateString(process.env.lang) || ''
-      },
-      async fetchPosts(
-          postType = this.postType,
-          amount = this.amount,
-          sortBy = this.sortBy,
-        ) {
-        return this.$content(postType)
-          .sortBy(sortBy.key, sortBy.direction)
-          .limit(amount)
-          .fetch()
-          .catch((err) => {
-            error({ statusCode: 404, message: amount > 1 ? 'Posts not found' : 'Post not found' })
-          });
-      }
-    },
-  }
+  },
+}
 </script>
+
+<style lang="postcss" scoped>
+.card-li {
+  position: relative;
+  width: calc(33% - 15px);
+  margin-top: 0;
+  margin-bottom: 15px;
+}
+.card-li:not(:nth-child(3n)) {
+  margin-right: 15px;
+}
+.card-li h3 {
+  font-weight: 600;
+  font-size: 17px;
+}
+.card-li p {
+  font-size: 15px;
+}
+.card-li h6 {
+  font-size: 10px;
+  position: absolute;
+  top: 0;
+  right: 0;
+  background: transparent;
+  color: var(--color-primary-100);
+  font-weight: 600;
+  background: var(--color-primary-700);
+  box-shadow: 3px 3px 15px -3px rgb(0 0 0 / 30%);
+  border-radius: 0.125rem;
+}
+
+.inc-docs__tags:not(:last-child) {
+  margin-bottom: 40px;
+}
+.inc-docs .cards {
+  height: 0;
+  overflow: hidden;
+  padding: 0;
+  transition: all 0.4s;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: flex-start;
+  align-items: flex-start;
+}
+.inc-docs__tags--active {
+  background: #f5f5f5;
+  border-radius: 0.25rem;
+}
+.inc-docs__tags--active .cards {
+  height: auto !important;
+  padding: 15px !important;
+  transition: all 0.4s;
+}
+.inc-docs__tags--active .inc-docs__title .inc-docs__title-icon {
+  transform: rotate(135deg);
+}
+
+.inc-docs__tag {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  padding: 15px 15px;
+}
+.inc-docs__tag small {
+  padding: 3px 5px;
+  background: var(--color-gray-900);
+  color: #fff;
+  border-radius: 0.125rem;
+}
+.inc-docs__tag small:not(:last-child) {
+  margin-right: 3px;
+}
+
+.inc-docs__title {
+  padding-bottom: 7px;
+  display: flex;
+  flex-flow: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+  background: var(--color-primary-800);
+  color: #fff;
+  border-radius: 0.25rem;
+  box-shadow: 3px 3px 15px -2px rgb(0 0 0 / 30%);
+  cursor: pointer;
+}
+.inc-docs__title-text {
+  font-size: 17px;
+}
+.inc-docs__title-icon {
+  font-size: 25px;
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  background: var(--color-primary-600);
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  transition: all 0.4s;
+  position: relative;
+  border: 0;
+}
+.inc-docs__title-icon span {
+  position: absolute;
+  top: -5px;
+}
+
+@media (max-width: 1024px) {
+  .card-li {
+    width: 100%;
+  }
+  .card-li:not(:nth-child(3n)) {
+    margin-right: unset;
+  }
+  .card-li:last-child {
+    margin-bottom: 0;
+  }
+  .card-li .card {
+    padding-top: 1.5rem;
+    padding-bottom: 2.2rem;
+  }
+}
+</style>
